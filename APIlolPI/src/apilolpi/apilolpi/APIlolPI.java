df09a700821915a586a672795442fe5a57f43fb8
Victor Eleuterio Trindade <eleuteriotrindade@gmail.com>
2017-11-20 14:03:11 -0200

    att com matches

--- APIlolPI/src/apilolpi/apilolpi/APIlolPI.java
+++ APIlolPI/src/apilolpi/apilolpi/APIlolPI.java
@@ -18,22 +18,38 @@ public class APIlolPI {
         String summonerName = JOptionPane.showInputDialog("Digite seu nome de invocador");
         String server = JOptionPane.showInputDialog("Digite seu servidor");
         
-        URL lolApiSite;
-        lolApiSite = new URL("https://"+server+"1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"+summonerName+"?api_key=RGAPI-d47a10c3-04ff-4432-a2db-101fbd42bb75");
+        URL lolApiName = new URL("https://"+server+"1.api.riotgames.com/lol/summoner/v3/summoners/by-name/"+summonerName+"?api_key=RGAPI-d47a10c3-04ff-4432-a2db-101fbd42bb75");
         
-        BufferedReader in = new BufferedReader(new InputStreamReader(lolApiSite.openStream()));
-        String lerApi = in.readLine();
+        BufferedReader readName = new BufferedReader(new InputStreamReader(lolApiName.openStream()));
+        String lerApi = readName.readLine();
         
-        System.out.println(lerApi);
+        
         
         JSONObject ApiSN = new JSONObject(lerApi);
         
+        String nome = ApiSN.getString("name");
+        int id = ApiSN.getInt("id");
+        int level = ApiSN.getInt("summonerLevel");
         
-        System.out.println("nome de invocador: " + (ApiSN.getString("name")));
-        System.out.println("id do invocador: " + (ApiSN.getInt("id")));
-        System.out.println("level do invocador: " + (ApiSN.getInt("summonerLevel")));
+        URL lolApiMatches = new URL("https://"+server+"1.api.riotgames.com/lol/match/v3/matchlists/by-account/"+ApiSN.getInt("accountId")+"?api_key=RGAPI-d47a10c3-04ff-4432-a2db-101fbd42bb75");
+        BufferedReader readMatches = new BufferedReader(new InputStreamReader(lolApiMatches.openStream()));
+        String lerApi2 = readMatches.readLine();
+        
+        JSONObject ApiMCH = new JSONObject(lerApi2);
+        
+        System.out.println("nome de invocador: " + nome);
+        System.out.println("id do invocador: " + id);
+        System.out.println("level do invocador: " + level);
+        
+        System.out.println(lerApi2);
+        
+        
+        
+        
         
 
     }
+
+    
     
 }
